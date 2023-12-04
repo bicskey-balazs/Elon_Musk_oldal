dd8bdd63b218cc1f44dd268e5873e1167885f40d
Geher-Marcell <geher.marcell@students.jedlik.eu>
2023-12-04 08:59:14 +0100

    cursor-fix, átnevezések, stb

--- cursor.js
+++ cursor.js
@@ -1,12 +1,19 @@
 const cursorRounded = document.querySelector('.rounded');
 
+var lastX;
+var lastY;
+
 const moveCursor = (e) => {
     
-    const mouseY = e.clientY + window.scrollY;
-    const mouseX = e.clientX + window.scrollX;
+    if (e.clientX != undefined) lastX = e.clientX;
+    if (e.clientY != undefined) lastY = e.clientY;
 
+    const mouseY = lastY + window.scrollY;
+    const mouseX = lastX + window.scrollX;
+    
     cursorRounded.style.top = mouseY + 'px';
     cursorRounded.style.left = mouseX + 'px';
 }
 
-window.addEventListener('mousemove', moveCursor)
+window.addEventListener('mousemove', moveCursor);
+window.addEventListener('scroll', moveCursor);
