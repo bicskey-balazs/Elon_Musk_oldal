1c4e8f40febf3933754e3bea7f71c3d50c993ad0
Geher-Marcell <geher.marcell@students.jedlik.eu>
2023-12-05 10:49:36 +0100

    cursor csak akkor van ha importolod a scriptjét

--- cursor.js
+++ cursor.js
@@ -1,7 +1,9 @@
-const cursorRounded = document.querySelector('.cursor');
+var cursor;
 
 var lastX;
 var lastY;
+
+cursorHideElements = ["body", "a"];
 
 const moveCursor = (e) => {
     
@@ -11,9 +13,30 @@ const moveCursor = (e) => {
     const mouseY = lastY + window.scrollY;
     const mouseX = lastX + window.scrollX;
     
-    cursorRounded.style.top = mouseY + 'px';
-    cursorRounded.style.left = mouseX + 'px';
+    cursor.style.top = mouseY + 'px';
+    cursor.style.left = mouseX + 'px';
 }
+
+onload = () => {
+    cursor = document.createElement('div');
+    cursor.style.width = '20px';
+    cursor.style.height = '20px';
+    cursor.style.border = '2px solid #fff';
+    cursor.style.borderRadius = '10% 50% 50% 50%';
+    cursor.style.position = 'absolute';
+    cursor.style.top = '0';
+    cursor.style.left = '0';
+    cursor.style.pointerEvents = 'none';
+    cursor.id = 'cursor';
+    document.body.appendChild(cursor);
+
+    for (var i = 0; i < cursorHideElements.length; i++) {
+        for(var j = 0; j < document.querySelectorAll(cursorHideElements[i]).length; j++) {
+            document.querySelectorAll(cursorHideElements[i])[j].classList.add("hidden-cursor");
+        }
+    }
+
+};
 
 window.addEventListener('mousemove', moveCursor);
 window.addEventListener('scroll', moveCursor);
--- script.js
+++ script.js
@@ -3,10 +3,8 @@ var links = document.querySelectorAll('a');
 for (var i = 0; i < links.length; i++) {
     links[i].addEventListener('mouseenter', function() {
         this.style.setProperty("--underline-width", "100%");
-        console.log(this);
     });
     links[i].addEventListener('mouseleave', function() {
-        console.log("leave");
         this.style.setProperty("--underline-width", "0%");
     });    
 }
