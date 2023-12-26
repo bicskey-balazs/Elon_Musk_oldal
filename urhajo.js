1926ddba2f2cb25ac678c296c0a8adf628ac7565
bicskey-balazs <bicskey.balazs@students.jedlik.eu>
2023-12-26 17:15:19 +0100

    urhajo

--- urhajo.js
+++ urhajo.js
@@ -1,3 +1,20 @@
-let csillagok = document.querySelector('csillagok');
-let hatter = document.querySelector('urJatek');
+let urhajo = [document.querySelector('.urhajoResz1'), document.querySelector('.urhajoResz2'), document.querySelector('.urhajoResz3')];
 
+document.querySelector('.urhajoResz1').style.left = "562px";
+document.querySelector('.urhajoResz2').style.left = "600px";
+document.querySelector('.urhajoResz3').style.left = "640px";
+
+document.addEventListener('keydown', function(e) {
+    if (e.key == 'ArrowLeft') {
+        if (parseInt(document.querySelector('.urhajoResz1').style.left) > 350) mozgas(-10);
+    }
+    else if (e.key == 'ArrowRight') {
+        if (parseInt(document.querySelector('.urhajoResz1').style.left) < 845) mozgas(10);
+    }
+});
+
+function mozgas(irany) {
+    urhajo.forEach(resz => {
+        resz.style.left = `${parseInt(resz.style.left) + irany}px`;
+    });
+}
