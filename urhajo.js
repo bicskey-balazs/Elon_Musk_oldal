fd21a5de29c3fce1d8ad7ed300e4bd6d41bf9fe5
bicskey-balazs <bicskey.balazs@students.jedlik.eu>
2023-12-28 20:21:23 +0100

    urhajos_jatek

--- urhajo.js
+++ urhajo.js
@@ -4,6 +4,7 @@ let kmCelKiiras = document.querySelector('.kmCel');
 function delay(mennyi) {
     return new Promise(resolve => setTimeout(resolve, mennyi));
 }
+let sebesség = 1500;
 let jatekvege = false;
 let aktKM = 0;
 let kmCel = 384000;
@@ -13,7 +14,7 @@ async function kmSzamlalo(cel) {
             jatekvege = true
         }
         await delay(1000);
-        aktKM += 1000;
+        aktKM += sebesség;
         kmKiiras.innerHTML = `km: ${aktKM}`
     }
 }
@@ -101,25 +102,92 @@ async function meteorMozgas(melyik) {
     }
 }
 //
+//boost
+let boost = document.querySelector('.boost');
+boost.style.display = "none";
+boost.style.top = "0";
+let boostSeged = 0;
+let boostSeged2 = true;
+async function boostCsinal() {
+    if (boostSeged == 3 && boostSeged2) {
+        boostSeged2 = false
+        rnd = Math.floor(Math.random() * 495) + 350;
+        boost.style.top = "0";
+        boost.style.left = `${rnd}px`;
+        boost.style.display = "inline";
+
+        while(boostSeged != 0) {
+            await delay(10);
+            if(parseInt(boost.style.top) < 650) {
+                boost.style.top = `${parseInt(boost.style.top) + 3}px`
+            }
+            else {
+                boost.style.display = "none";
+                boost.style.top = "0";
+                boostSeged2 = true;
+                boostSeged = 0;
+            }
+        }
+    }
+    else boostSeged++;
+}
+
+//
 //játék
+let holdGomb = document.querySelector('.holdGomb');
+let marsGomb = document.querySelector('.marsGomb');
+holdGomb.addEventListener("mouseup", jatekHold);
+marsGomb.addEventListener("mouseup", jatekMars);
+
 let hatter = document.querySelector('#urHatter');
 let startGomb = document.querySelector('.startGomb');
-startGomb.addEventListener("mouseup", jatek);
+startGomb.addEventListener("mouseup", bolygoValasztas);
 
 document.querySelector('#urhajo').style.display = "none";
 kmKiiras.style.display = "none";
 kmCelKiiras.style.display = "none";
 
-async function jatek() {
-    hatter.style.backgroundImage = "url(képek/csillagok8.gif)";
-    kmKiiras.style.display = "inline";
-    kmCelKiiras.style.display = "inline";
+document.querySelector('.bolygoValasztas').style.display = "none";
+function bolygoValasztas() {
     startGomb.style.display = "none";
     document.querySelector('.tippek').style.display = "none";
+    document.querySelector('.bolygoValasztas').style.display = "inline";
+}
+
+async function jatekHold() {
+    sebesség = 1500
+    kmCel = 384000
+    kmCelKiiras.innerHTML = `Cél: ${kmCel}km`;
+    hatter.style.backgroundImage = "url(képek/csillagok8.gif)";
+    startGomb.style.display = "none";
+    document.querySelector('.tippek').style.display = "none";
+    document.querySelector('.bolygoValasztas').style.display = "none";
     document.querySelector('#urhajo').style.display = "inline";
+    kmKiiras.style.display = "inline";
+    kmCelKiiras.style.display = "inline";
     kmSzamlalo();
     while(!jatekvege) {
-        await delay(1500);
+        await delay(1200);
+        boostCsinal();
+        meteorCsinal();
+    }
+}
+
+async function jatekMars() {
+    sebesség = 250000
+    kmCel = 100;
+    kmCelKiiras.innerHTML = `Cél: ${kmCel}millió km`;
+    hatter.style.backgroundImage = "url(képek/csillagok8.gif)";
+    startGomb.style.display = "none";
+    document.querySelector('.tippek').style.display = "none";
+    document.querySelector('.bolygoValasztas').style.display = "none";
+    document.querySelector('#urhajo').style.display = "inline";
+    kmKiiras.style.display = "inline";
+    kmCelKiiras.style.display = "inline";
+    kmSzamlalo();
+    while(!jatekvege) {
+        await delay(1200);
+        boostCsinal();
         meteorCsinal();
     }
 }
